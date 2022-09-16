108734dc8dbb924a412c755087601cef2db4ad60
Irykson <Luca.Peterschilka@gmail.com>
2022-09-16 22:48:29 +0200

    Initial setup

---
+++ client/src/lib/components/Input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let variant: 'primary' | 'secondary' | undefined = undefined;
+</script>
+
+<input
+	class="input input-bordered"
+	class:input-primary={variant === 'primary'}
+	class:input-secondary={variant === 'secondary'}
+	{...$$restProps}
+/>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Input from '$lib/components/Input.svelte';
+</script>
+
+<Input type="text" variant="primary" placeholder="Invite Password" />
